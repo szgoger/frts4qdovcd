084a5315c209771853afee7e1262f44f1c3ba9d7
szgoger <78360181+szgoger@users.noreply.github.com>
2022-06-21 09:50:23 +0200

    quadrupole field

--- confined_field.py
+++ confined_field.py
@@ -20,7 +20,7 @@ def apply_confinement_and_field(molecule,E,l):
     mf.kernel()
     conf = cc.UCCSD(mf)
     conf.kernel()
-    r2_conf = get_rn_expt(molecule,conf,grid,2) # Calculating r2 in confimenent (note: no field here)
+    r2_conf = get_rn_expt(molecule,conf,grid,3) # Calculating r2 in confimenent (note: no field here)
     confined_energy = conf.e_tot
 
     h=(h+ np.einsum('x,xij->ij', field, molecule.intor('cint1e_r_sph', comp=3))) # Adding field to the confined atom
@@ -31,6 +31,33 @@ def apply_confinement_and_field(molecule,E,l):
     conf_field = cc.UCCSD(mf).run()
     confined_field_energy = conf_field.e_tot
     return confined_energy, confined_field_energy, r2_conf
+
+# The main function, handles the external field and the confinement
+def apply_confinement_and_quadrupole_field(molecule,E,l):
+    field=[E/2.0,0,0] # X field
+    molecule.set_common_orig([0, 0, 0])  # Not sure if we need it...?
+
+    h=(molecule.intor('cint1e_kin_sph') + molecule.intor('cint1e_nuc_sph') + l* molecule.intor('cint1e_r2_sph')) #Adding a confinement l*r^2
+    mf = scf.UHF(molecule)
+    mf.get_hcore = lambda *args: h
+    mf.scf()
+    mf.kernel()
+    conf = cc.UCCSD(mf)
+    conf.kernel()
+    r2_conf = get_rn_expt(molecule,conf,grid,2) # Calculating r2 in confimenent (note: no field here)
+    r4_conf = get_rn_expt(molecule,conf,grid,4) # Calculating r2 in confimenent (note: no field here)
+    confined_energy = conf.e_tot
+
+    h=(h+3.0*np.einsum('x,xij->ij', field, mol.intor('int1e_zz', comp=3))-np.einsum('x,xij->ij', field, mol.intor('cint1e_r2_sph', comp=3))) # Adding field to the confined atom
+    mf = scf.UHF(molecule)
+    mf.get_hcore = lambda *args: h
+    mf.scf()
+    mf.kernel()
+    conf_field = cc.UCCSD(mf).run()
+    confined_field_energy = conf_field.e_tot
+    return confined_energy, confined_field_energy, r2_conf, r4_conf
+
+# Real space evaluation of r^n matrix element
 
 # Real space evaluation of r^n matrix element
 def get_rn_expt(molecule,mycc,grids,n):
@@ -48,10 +75,10 @@ def get_rn_expt(molecule,mycc,grids,n):
 # Setting up the molecule
 mol = gto.Mole()
 mol.atom = '''
-     He 0.0000 0.0000     0.000000000000
+     Na 0.0000 0.0000     0.000000000000
   '''
 mol.basis = 'aug-cc-pvQz'
-mol.spin=0
+mol.spin=1
 mol.build()
 
 # Unperturbed calculators
@@ -65,9 +92,11 @@ grid = pyscf.dft.gen_grid.Grids(mol)
 grid.level = 8
 grid.build()
 
-confinements = np.arange(0,5,0.1)
+results = open("conf_alpha2_r2_r4.txt", "w")
+
+confinements = np.arange(0,0.2,0.001)
 for conf in confinements:
-    ene0, ene1, r2 = apply_confinement_and_field(mol,0.001,conf)
-    alpha = -2*(ene1-ene0)/0.001/0.001
-    print(conf,alpha, r2)
+    ene0, ene1, r2, r4 = apply_confinement_and_quadrupole_field(mol,0.0001,conf)
+    alpha = -3*(ene1-ene0)/0.0001/0.0001
+    results.write(str(conf) + "  "+ str(alpha) + "   "+ str(r2) +"  "+str(r4)+ "\n")
 
