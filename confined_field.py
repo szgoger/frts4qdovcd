d890a609993fe466b758cbcc05edb935ff1996a5
szgoger <78360181+szgoger@users.noreply.github.com>
2022-07-13 15:33:37 +0200

    Update confined_field.py

--- confined_field.py
+++ confined_field.py
@@ -10,6 +10,17 @@ from pyscf import gto, scf, tools, cc, dft
 from scipy.special import sph_harm
 import math as m
 
+# Setting up the molecule
+mol = gto.Mole()
+mol.atom = '''
+     H 0.0000 0.0000     0.000000000000
+  '''
+mol.basis = 'aug-cc-pvtz'
+mol.spin=1
+mol.charge=0
+mol.build()
+mol.verbose=0
+
 # Simple coordinate conversion
 def cart2sph(coord):
     x,y,z = coord
@@ -29,8 +40,8 @@ def gen_matrix_element(function_to_eval, molecule, mycc, args):
         matelements = np.append(matelements, real_space_value)
 
     dm1 = mycc.make_rdm1(ao_repr=True)[0] + mycc.make_rdm1(ao_repr=True)[1]
-    ao_value =  dft.numint.eval_ao(mol, grid.coords, deriv=0)
-    rho = dft.numint.eval_rho(mol, ao_value, dm1)
+    ao_value =  dft.numint.eval_ao(molecule, grid.coords, deriv=0)
+    rho = dft.numint.eval_rho(molecule, ao_value, dm1)
 
     combined = grid.weights * matelements
     integral = np.dot(rho.T, combined.T)
@@ -42,14 +53,18 @@ def apply_confinement_and_field(molecule,E,l):
     field=[E,0,0] # X field
     molecule.set_common_orig([0, 0, 0])  # Not sure if we need it...?
 
-    h=(molecule.intor('cint1e_kin_sph') + molecule.intor('cint1e_nuc_sph') + l* molecule.intor('cint1e_r2_sph')) #Adding a confinement l*r^2
+    relative_confinement = np.abs(molecule.intor('cint1e_kin_sph').trace() + molecule.intor('cint1e_nuc_sph').trace())*0.01*l
+
+    h=(molecule.intor('cint1e_kin_sph') + molecule.intor('cint1e_nuc_sph') + relative_confinement* molecule.intor('cint1e_r2_sph') - 0.0000 * molecule.intor('cint1e_z_sph').trace()) #Adding a confinement l*r^2
     mf = scf.UHF(molecule)
     mf.get_hcore = lambda *args: h
     mf.scf()
     mf.kernel()
     conf = cc.UCCSD(mf)
     conf.kernel()
-    r2_conf = get_rn_expt(molecule,conf,grid,3) # Calculating r2 in confimenent (note: no field here)
+    r2_conf =  gen_matrix_element(rn, mol, conf,2)
+    r3_conf =  gen_matrix_element(rn, mol, conf,3)
+    alpha_conf = polarizability_lm_lpmp(mol, conf, 1, 0, 1, 0) #dipole alpha
     confined_energy = conf.e_tot
 
     h=(h+ np.einsum('x,xij->ij', field, molecule.intor('cint1e_r_sph', comp=3))) # Adding field to the confined atom
@@ -59,7 +74,7 @@ def apply_confinement_and_field(molecule,E,l):
     mf.kernel()
     conf_field = cc.UCCSD(mf).run()
     confined_field_energy = conf_field.e_tot
-    return confined_energy, confined_field_energy, r2_conf
+    return confined_energy, confined_field_energy, alpha_conf, r2_conf, r3_conf
 
 # The main function, handles the external field and the confinement
 def apply_confinement_and_quadrupole_field(molecule,E,l):
@@ -73,8 +88,8 @@ def apply_confinement_and_quadrupole_field(molecule,E,l):
     mf.kernel()
     conf = cc.UCCSD(mf)
     conf.kernel()
-    r2_conf = get_rn_expt(molecule,conf,grid,2) # Calculating r2 in confimenent (note: no field here)
-    r4_conf = get_rn_expt(molecule,conf,grid,4) # Calculating r2 in confimenent (note: no field here)
+    r2_conf =  gen_matrix_element(rn, molecule, coupledcluster,2) # Calculating r2 in confimenent (note: no field here)
+    r4_conf =  gen_matrix_element(rn, molecule, coupledcluster,4) # Calculating r2 in confimenent (note: no field here)
     confined_energy = conf.e_tot
 
     h=(h+3.0*np.einsum('x,xij->ij', field, mol.intor('int1e_zz', comp=3))-np.einsum('x,xij->ij', field, mol.intor('cint1e_r2_sph', comp=3))) # Adding field to the confined atom
@@ -86,14 +101,6 @@ def apply_confinement_and_quadrupole_field(molecule,E,l):
     confined_field_energy = conf_field.e_tot
     return confined_energy, confined_field_energy, r2_conf, r4_conf
 
-# Setting up the molecule
-mol = gto.Mole()
-mol.atom = '''
-     Ne 0.0000 0.0000     0.000000000000
-  '''
-mol.basis = 'aug-cc-pvQz'
-mol.spin=0
-mol.build()
 
 # Unperturbed calculators
 mf0 = scf.UHF(mol)
@@ -103,7 +110,7 @@ cc0.kernel()
 
 # Grid for spatial integration
 grid = pyscf.dft.gen_grid.Grids(mol)
-grid.level = 9
+grid.level = 5
 grid.build()
 
 # Input functions for general matrix elements
@@ -122,20 +129,32 @@ def solid_harmonic_squared(r, theta, phi, args):
     return one_harmonic * one_harmonic
 
 def polarizability_lm_lpmp(molecule, coupledcluster, l, m, lp, mp): #returns \alpha_lml'm'
-    prefactor = 16*np.pi/(2*l+1)
+    #nr_of_electrons = gen_matrix_element(rn, molecule, coupledcluster,0)
+    nr_of_electrons = 1
+    prefactor = 4*nr_of_electrons*4*np.pi/(2*l+1)
+
     c_expt_value = gen_matrix_element(c_operator, molecule, coupledcluster, (lp, mp))
     field_squared = gen_matrix_element(solid_harmonic_squared, molecule, coupledcluster, (lp, mp))
     response_squared = gen_matrix_element(solid_harmonic_squared, molecule, coupledcluster, (l, m))
     return prefactor*field_squared*response_squared/c_expt_value
 
-print(polarizability_lm_lpmp(mol, cc0, 1, 0, 1, 0))
+#print(polarizability_lm_lpmp(mol, cc0, 1, 0, 1, 0))
 
 #results = open("conf_alpha2_r2_r4.txt", "w")
 
-#confinements = np.arange(0,0.2,0.001)
-#for conf in confinements:
-#    ene0, ene1, r2, r4 = apply_confinement_and_quadrupole_field(mol,0.0001,conf)
-#    alpha = -3*(ene1-ene0)/0.0001/0.0001
-#    results.write(str(conf) + "  "+ str(alpha) + "   "+ str(r2) +"  "+str(r4)+ "\n")
+field = 0.005
+confinements = np.arange(0,1,0.05)
+r2_0, r3_0, al4_0, a0 = "k", "k", "k", "k"
+for conf in confinements:
+    ene0, ene1, alpha_l4, r2_conf, r3_conf = apply_confinement_and_field(mol,field,conf)
+    alpha = -2*(ene1-ene0)/field/field
+    if r2_0 == "k":
+        r2_0 = r2_conf
+        r3_0 = r3_conf
+        al4_0 = alpha_l4
+        a0 = alpha
+    #alpha = -3*(ene1-ene0)/0.0001/0.0001
+    #results.write(str(conf) + "  "+ str(alpha) + "   "+ str(r2) +"  "+str(r4)+ "\n")
+    print(conf, alpha, str(a0*np.real(alpha_l4)/np.real(al4_0)/alpha), a0*r2_conf**2/r2_0**2/alpha, a0*r3_conf /r3_0/alpha)
 
 
